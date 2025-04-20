64fb5efdf3de093f20f228a694e064c0bd1a1355
WesselBoi <mathur.saksham2005@gmail.com>
2025-04-20 21:15:09 +0530

    added patch to cors

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(cors({
     origin: process.env.CLIENT_URL || 'https://blogbyte-wuzs.onrender.com',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']
 }))
 
 app.use(express.json())
